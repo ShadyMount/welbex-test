144ad758173a1cfb96fb1414353a6d8844214849
ShadyMount <shady2142@gmail.com>
2022-07-19 22:02:05 +0300

    fix: find items log deleted

--- server/services/itemsService.js
+++ server/services/itemsService.js
@@ -4,7 +4,6 @@ const items = require("../settings/db").items
 class TasksService {
 
     async findItems({ sort = 'all', currentPage, pageSize }) {
-        console.log(items);
         const skip = (currentPage - 1) * pageSize
         const itemsFromDb = await items.findAll({
             // where: whereConditions(sort),
