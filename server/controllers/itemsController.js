81dc7e7190b1eed520f6544b86ada9b579be1297
ShadyMount <shady2142@gmail.com>
2022-07-20 16:29:18 +0300

    feat: sort added

--- server/controllers/itemsController.js
+++ server/controllers/itemsController.js
@@ -6,87 +6,41 @@ const itemsService = require('../services/itemsService')
 
 class tasksController {
 
-
     async getItems (req, res, next){
-            console.log('controller req: ', req.query);
             const currentPage = Number(req.query.page) || 1
             const pageSize = Number(req.query.pagesize) || 15 
             const sort = req.query?.sort
+            const sortValue = req.query?.sortValue
+            const sortDir = req.query?.sortDir
             try {
-                const items = await itemsService.findItems({sort, currentPage, pageSize})  
-                response.status(200, items, res)
+                const items = await itemsService.findItems({sort, sortValue, sortDir, currentPage, pageSize})
+                const itemsAmount = await itemsService.countItems({sort, sortValue, sortDir})     
+                response.status(200, {items, itemsAmount}, res)
             } catch (e) {
                 next(e);
             }
-            
     }
 
-    // async countTasks (req, res, next){
-    //     try {
-    //         const status = req.query?.status
-    //         const master = req.query?.master
-    //         const date = req.query?.date 
-    //         const totalTasksCount = await TasksService.countTasks(status, date, master)   
-    //         console.log('totalTasksCount: ', totalTasksCount);
-    //         response.status(200, totalTasksCount, res)
-    //     } catch (e) {
-    //         next(e);
-    //     }
-    // }
-
-
-    // async sumOfTasksByDateRange(req, res, next){
-    //     try {
-    //         const master = req.query.master
-    //         const startdate = req.query.startdate
-    //         const finishdate = req.query.finishdate
-    //         const sum = await TasksService.sumOfTasksByDateRange(master, startdate, finishdate)
-
-    //         response.status(200, sum, res)
-    //     } catch (e) {
-    //         next(e)
-    //     }
-    // }
+    async countItems (req, res, next){
+        try {
+            const sort = req.query?.sort
+            const sortValue = req.query?.sortValue
+            const sortDir = req.query?.sortDir
+            const itemsAmount = await itemsService.countItems({sort, sortValue, sortDir})   
+            response.status(200, itemsAmount, res)
+        } catch (e) {
+            next(e);
+        }
+    }
 
     // async add (req, res, next){
     //     try {
-    //         await TasksService.addTask(req.body)
-    //         console.log('req.body: ', req.body);
-    //         await longpoll.publish("/poll", { message: "task added" })
-    //         response.status(200, {message: 'task added'}, res)
+    //         await itemsService.addTask(req.body)
+    //         response.status(200, {message: 'item added'}, res)
     //     } catch (e) {
     //         next(e);
     //     }
     // }
-
-
-
-    // async update (req, res, next){
-
-    //     try {
-    //         console.log('BODY: ', req.body);
-    //         await TasksService.updateTask(req.body)
-    //         await longpoll.publish("/poll", { message: `task ${req.body.id} updated` })
-    //         response.status(200, {message: `task ${req.body.id} updated`}, res)
-    //     } catch (e) {
-    //         next(e);
-    //     }
-
-    // }
-
-    // async updateTaskMaster (req, res, next){
-
-    //     try {
-    //         await TasksService.updateTaskMaster(req.query.master, req.query.masterId)
-    //         await longpoll.publish("/poll", { message: `task ${req.body.id} updated` })
-    //         response.status(200, {message: `task updated`}, res)
-    //     } catch (e) {
-    //         next(e);
-    //     }
-
-    // }
-
-    
 
 }
 
--- server/services/itemsService.js
+++ server/services/itemsService.js
@@ -1,12 +1,54 @@
+const { Op } = require("sequelize")
+
 const items = require("../settings/db").items
 
+const whereConditions = (sort, sortValue, sortDir) => {
+    let conditions = {}
+
+    if(sort === 'name'){
+        if(sortDir === 'contains'){
+            conditions = {...conditions, 'name': {[Op.substring]: sortValue}}
+        }else{
+            conditions = {...conditions, 'name': {[Op.eq]: sortValue}}
+        }
+    }
+    if(sort === 'quantity'){
+        if(sortDir === 'more'){
+            conditions = {...conditions, 'quantity': {[Op.gt]: sortValue}}
+        }
+        if(sortDir === 'less'){
+            conditions = {...conditions, 'quantity': {[Op.lt]: sortValue}}
+        }
+        if(sortDir === 'contains'){
+            conditions = {...conditions, 'quantity': {[Op.substring]: sortValue}}
+        }
+        else{
+            conditions = {...conditions, 'quantity': {[Op.eq]: sortValue}}
+        }
+    }
+    if(sort === 'distance'){
+        if(sortDir === 'more'){
+            conditions = {...conditions, 'distance': {[Op.gt]: sortValue}}
+        }
+        if(sortDir === 'less'){
+            conditions = {...conditions, 'distance': {[Op.lt]: sortValue}}
+        }
+        if(sortDir === 'contains'){
+            conditions = {...conditions, 'distance': {[Op.substring]: sortValue}}
+        }
+        else{
+            conditions = {...conditions, 'distance': {[Op.eq]: sortValue}}
+        }
+    }
+    return conditions
+}
 
 class TasksService {
 
-    async findItems({ sort = 'all', currentPage, pageSize }) {
+    async findItems({ sort, sortValue, sortDir, currentPage, pageSize }) {
         const skip = (currentPage - 1) * pageSize
         const itemsFromDb = await items.findAll({
-            // where: whereConditions(sort),
+            where: whereConditions(sort, sortValue, sortDir),
             offset: skip,
             limit: pageSize
         })
@@ -14,17 +56,16 @@ class TasksService {
         return itemsFromDb
     }
 
-        async countItems() {
-                const count = await items.count()
+        async countItems({ sort, sortValue, sortDir }) {
+                const count = await items.count({
+                    where: whereConditions(sort, sortValue, sortDir,)
+                })
                 return count
-    
         }
 
         async addItem(item) {
             await items.create(item)
         }
-
-
 }
 
 module.exports = new TasksService()
