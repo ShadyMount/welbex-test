5185400f89c49731e0211491cf89173b1958ef17
ShadyMount <shady2142@gmail.com>
2022-07-20 10:25:17 +0300

    filters created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
-import { Table } from "./components";
+import { useState } from "react";
+import { Filters, Table } from "./components";
 
 function App() {
+  const [sort, setSort] = useState(null)
+  const [sortValue, setSortValue] = useState(null)
+  const [sortDir, setSortDir] = useState(null)
+  
   return (
     <div className="App">
+      <Filters />
       <Table />
     </div>
   );
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,1 +1,2 @@
+export { Filters } from './Filters/Filters';
 export {Table} from './Table'
