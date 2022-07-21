d412d08a5bd39bce76d9ce79ef462efc1ea77ca7
ShadyMount <shady2142@gmail.com>
2022-07-21 14:16:30 +0300

    fix: server port changed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const port = process.env.APPPORT || 3001
+const port = process.env.PORT || 5000
 const routes = require('./settings/routes')
 
 const cors = require('cors');
